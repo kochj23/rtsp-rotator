a47c4b5a1112f195075818d3a29e80b57edbda2e
Jordan Koch <kochj@digitalnoise.net>
2025-10-10 11:29:44 -0700

    Initial Commit

---
+++ RTSP Rotator/RTSP_RotatorView.m
@@ -0,0 +1,51 @@
+//
+//  RTSP_RotatorView.m
+//  RTSP Rotator
+//
+//  Created by Jordan Koch on 10/10/25.
+//
+
+#import "RTSP_RotatorView.h"
+
+@implementation RTSP_RotatorView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
