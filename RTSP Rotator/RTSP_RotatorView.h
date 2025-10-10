8360300af24a3a405dc450f8163468d6b08778ab
Jordan Koch <kochj@digitalnoise.net>
2025-10-10 11:29:44 -0700

    Initial Commit

---
+++ RTSP Rotator/RTSP_RotatorView.h
@@ -0,0 +1,12 @@
+//
+//  RTSP_RotatorView.h
+//  RTSP Rotator
+//
+//  Created by Jordan Koch on 10/10/25.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface RTSP_RotatorView : ScreenSaverView
+
+@end
